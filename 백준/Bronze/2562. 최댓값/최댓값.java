8c054acc1bf65f9855a6384d553137c3008b8923
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-01-26 17:22:58 +0900

    [Bronze III] Title: 최댓값, Time: 172 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int max = arr[0];
+        int maxIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+            if (max <= arr[i]) {
+                max = arr[i];
+                maxIndex = i + 1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(maxIndex);
+    }
+}
