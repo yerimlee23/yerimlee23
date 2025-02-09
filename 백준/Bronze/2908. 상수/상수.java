00130cfbc621dbb1ab914c79daf19d5a4ed5627f
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-02-09 20:39:38 +0900

    [Bronze II] Title: 상수, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int answerA = 0, answerB = 0;
+
+        for (int i = 0; i < 3; i++) {
+            answerA = answerA * 10 + a % 10;
+            a /= 10;
+            answerB = answerB * 10 + b % 10;
+            b /= 10;
+        }
+
+        if (answerA > answerB) {
+            bw.write(answerA + "\n");
+        } else {
+            bw.write(answerB + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
