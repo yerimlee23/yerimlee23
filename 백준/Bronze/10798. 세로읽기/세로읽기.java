9aa799d76b11f8feeb122712af46350524effe14
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-02-13 23:23:36 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[][] arr = new String[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = String.valueOf(str.charAt(j));
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != null) {
+                    bw.write(arr[j][i]);
+                }
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
