cb1c17d311c60789beece9059252d94d567728da
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-03-03 22:44:54 +0900

    [Bronze III] Title: 대지, Time: 308 ms, Memory: 30236 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String arg[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] x = new int[N];
+        int[] y = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        int result = (x[N - 1] - x[0]) * (y[N - 1] - y[0]);
+
+        bw.write(result + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
