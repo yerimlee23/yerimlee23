3fb26e85327f9e82f10d522c60a275acb38e5df3
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-03-10 23:16:35 +0900

    [Silver V] Title: Pismo, Time: 284 ms, Memory: 25336 KB -BaekjoonHub

---
+++ 백준/Silver/16756. Pismo/Pismo.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < N; i++) {
+            min = Math.min(min, Math.abs(arr[i] - arr[i - 1]));
+        }
+
+        bw.write(min + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
