4796c78befaf762e27acee4bab1990a909165758
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-07-11 23:31:36 +0900

    [Bronze V] Title: AI 선도대학, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        bw.write(N - 2024 + "");
+        bw.close();
+    }
+}
