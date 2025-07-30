7421dc7aea24400493f37c700f3423b026e4d296
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-07-31 00:00:48 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
