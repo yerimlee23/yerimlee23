fc200ca18748d92cb46f6ada3dda705de2d9207d
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2025-10-21 01:43:51 +0900

    [Bronze V] Title: 제리와 톰, Time: 120 ms, Memory: 16068 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		br.close();
+		
+		int P = B - A;
+		int Q = B;
+		bw.write(P + " " + Q);
+		bw.close();
+	}
+}
