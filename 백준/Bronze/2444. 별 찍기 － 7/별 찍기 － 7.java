e00a0231cc793ea819a3ef86ca37ea76f7e9c360
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-03 00:24:31 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 116 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,29 +1,36 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < (N - 1 - i); j++) {
-                System.out.print(" ");
+            for (int j = i; j < N - 1; j++) {
+                bw.write(" ");
             }
-            for (int j = 0; j < (i * 2 + 1); j++) {
-                System.out.print("*");
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                bw.write("*");
             }
-            System.out.println();
+
+            bw.write("\n");
         }
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                bw.write(" ");
             }
-            for (int j = 0; j < (2 * N - (i * 2 + 1)); j++) {
-                System.out.print("*");
+
+            for (int j = 0; j < 2 * (N - i) - 3; j++) {
+                bw.write("*");
             }
-            System.out.println();
+
+            bw.write("\n");
         }
+
+        bw.flush();
     }
 }
