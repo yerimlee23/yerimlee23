64e79d652161011d4dbcb54d9ffcfe437bbb5ece
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-03 00:38:36 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 104 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                bw.write("*");
+            }
+
+            for (int j = 0; j < 2 * (N - i); j++) {
+                bw.write(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                bw.write("*");
+            }
+
+            bw.write("\n");
+        }
+
+        for (int i = 1; i <= N - 1; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                bw.write("*");
+            }
+
+            for (int j = 1; j <= 2 * i; j++) {
+                bw.write(" ");
+            }
+
+            for (int j = 1; j <= N - i; j++) {
+                bw.write("*");
+            }
+
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+}
