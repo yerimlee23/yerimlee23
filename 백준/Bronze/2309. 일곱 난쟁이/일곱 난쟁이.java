a916a2bdaa61889b4b65e2d776f8cca12fdca27f
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-16 02:07:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,37 +1,35 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] height = new int[9];
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            height[i] = Integer.parseInt(br.readLine());
-            sum += height[i];
-        }
-        br.close();
+		int[] height = new int[9];
+		int sum = 0;
+		
+		for (int i = 0; i < height.length; i++) {
+			height[i] = Integer.parseInt(br.readLine());
+			sum += height[i];
+		}
+		
+		for (int i = 0; i < height.length; i++) {
+			for (int j = i + 1; j < height.length; j++) {
+				if (sum - height[i] - height[j] == 100) {
+					height[i] = 0;
+					height[j] = 0;
+					
+					Arrays.sort(height);
 
-        boolean find = false;
-        for(int i = 0; i < 9 && !find; i++) {
-            for (int j = 1; j < 9; j++) {
-                if (sum - height[i] - height[j] == 100) {
-                    height[i] = 0;
-                    height[j] = 0;
-                    find = true;
-                    break;
-                }
-            }
-        }
-
-        Arrays.sort(height);
-        for (int i = 2; i < 9; i++) {
-            bw.write(height[i] + "\n");
-        }
-
-        bw.flush();
-        bw.close();
-    }
+					for (int k = 2; k < height.length; k++) {
+						bw.write(height[k] + "\n");
+					}
+					
+					bw.flush();
+					return;
+				}
+			}
+		}
+	}
 }
