91add85afc39dafee59091c57dfb2aa0c1f73390
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-16 01:53:16 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,23 +1,26 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String arg[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int check = 1;
-        int count = 1;
-
-        while (check < N) {
-            check += 6 * count;
-            count++;
-        }
-
-        bw.write(count + "\n");
-
-        br.close();
-        bw.flush();
-        bw.close();
-    }
+		int n = Integer.parseInt(br.readLine()); //목표 방 번호
+		
+		int cur = 1; //현재 위치 (갈 수 있는 방 범위)
+		int visit = 1; //방문한 방 개수(1번 방부터 시작하므로 1로 초기화)
+		
+		while (true) {
+			if (cur >= n) {
+				break;
+			}
+			
+			cur += 6 * visit;
+			visit++;
+		}
+		 
+		bw.write(visit + "");
+		bw.flush();
+	}
 }
