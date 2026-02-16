2d5315ce42b52f86f2ec30dba4490d0b7eb2fcec
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-02-16 14:46:31 +0900

    [Silver III] Title: 색종이 - 2, Time: 104 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -6,44 +6,43 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int[][] map = new int[100][100];
-		int n = Integer.parseInt(br.readLine());
-		
+		int n = Integer.parseInt(br.readLine()); // 색종이 수
+		int[][] map = new int[101][101]; // 도화지 크기
+
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			
+
 			for (int j = x; j < x + 10; j++) {
 				for (int k = y; k < y + 10; k++) {
-					if (0 <= x && x < 100 && 0 <= y && y < 100) {
-						map[j][k]++;
-					}
+					map[j][k] = 1;
 				}
 			}
 		}
-		
-		//동서남북
-			int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-			int sum = 0;
-			
-			for (int i = 0; i < 100; i++) {
-				for (int j = 0; j < 100; j++) {
-					if (map[i][j] != 0) { //색종이 영역
-						for (int k = 0; k < d.length; k++) {
-							int a = i + d[k][0]; //행 변화(위아래)
-							int b = j + d[k][1]; //열 변화(좌우)
-							
-							//색종이 둘레 구하기
-							if (a < 0 || a >= 100 || b < 0 || b >= 100 || map[a][b] == 0) {
-								sum++;
-							}
+
+		int len = 0; // 색종이 둘레
+
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				if (map[i][j] == 1) { // 색종이 영역일 때
+					// 북동남서
+					int[] dr = { -1, 0, 1, 0 }; // 행(위아래)
+					int[] dc = { 0, 1, 0, -1 }; // 열(좌우)
+
+					for (int k = 0; k < 4; k++) { //좌표 주변 사방 탐색
+						int nr = i + dr[k]; //다음 행 좌표
+						int nc = j + dc[k]; //다음 열 좌표
+
+						if (nr < 0 || nr >= map.length || nc < 0 || nc >= map.length || map[nr][nc] == 0) { //사방 탐색했을 때 도화지나 색종이 영역이 아니라면
+							len++; //색종이 둘레이다
 						}
 					}
 				}
 			}
-			
-			bw.write(sum + "");
-			bw.flush();
+		}
+
+		bw.write(len + "");
+		bw.flush();
 	}
 }
