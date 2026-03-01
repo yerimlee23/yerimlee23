f253ca8999f8370c1d572f69b4278a44499377db
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-03-02 01:43:19 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 96 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,48 +1,74 @@
-//DP bottom-top 방식
+//DP top-down 방식
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int n;
+	static int[][] map;
+	static long[][][] memo;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int n = Integer.parseInt(br.readLine());
-		int[][] map = new int[n + 1][n + 1];
+		n = Integer.parseInt(br.readLine());
+		map = new int[n + 1][n + 1];
+		memo = new long[n + 1][n + 1][3]; //r, c, dir
 		
 		for (int i = 1; i < n + 1; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < n + 1; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				Arrays.fill(memo[i][j], -1);
 			}
 		}
 		
-		long[][][] dp = new long[n + 1][n + 1][3]; //r, c, dir
-		dp[1][2][0] = 1; //(1, 1), (1, 2)
+		long result = dp(1, 2, 0); //r, c, dir
 		
-		for (int r = 1; r < n + 1; r++) {
-			for (int c = 3; c < n + 1; c++) {
-				if (map[r][c] == 1) {
-					continue;
-				}
-				
-				//가로 = 우 + 우하
-				dp[r][c][0] = dp[r][c - 1][0] + dp[r][c - 1][2];
-						
-				//세로 = 하 + 우하
-				dp[r][c][1] = dp[r - 1][c][1] + dp[r - 1][c][2];
-						
-				//대각선 = 우 + 하
-				if (map[r - 1][c] == 0 && map[r][c - 1] == 0) {
-					dp[r][c][2] = dp[r - 1][c - 1][0] + dp[r - 1][c - 1][1] + dp[r - 1][c - 1][2];
-				}
-			}
-		}
-		
-		long count = dp[n][n][0] + dp[n][n][1] + dp[n][n][2];
-		
-		bw.write(count + "");
+		bw.write(result + "");
 		bw.flush();
+	}
+	
+	public static long dp(int r, int c, int dir) {
+		if (r == n && c == n) {
+			return 1;
+		}
+		
+		if (memo[r][c][dir] != -1) {
+			return memo[r][c][dir];
+		}
+		
+		long count = 0;
+		
+		switch(dir) {
+		case 0: //가로
+			if (c + 1 <= n && map[r][c + 1] == 0) {
+				count += dp(r, c + 1, 0); //우
+			}
+			break;
+		case 1: //세로
+			if (r + 1 <= n && map[r + 1][c] == 0) {
+				count += dp(r + 1, c, 1); //하
+			}
+			break;
+		case 2: //대각선
+			if (c + 1 <= n && map[r][c + 1] == 0) {
+				count += dp(r, c + 1, 0); //우
+			}
+			
+			if (r + 1 <= n && map[r + 1][c] == 0) {
+				count += dp(r + 1, c, 1); //하
+			}
+			break;
+		}
+		
+		//대각선
+		if (r + 1 <= n && c + 1 <= n && map[r + 1][c] == 0 && map[r][c + 1] == 0 && map[r + 1][c + 1] == 0) {
+			count += dp(r + 1, c + 1, 2); //우하
+		}
+		
+		memo[r][c][dir] = count;
+		return count;
 	}
 }
