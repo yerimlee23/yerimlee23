02d963e079f9844170ecc7db91144677cf9dc4f2
yerimlee23 <110087741+yerimlee23@users.noreply.github.com>
2026-03-09 01:19:31 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int n = Integer.parseInt(br.readLine());
+		long result = 1;
+		
+		for (int i = 2; i <= n; i++) {
+			result *= i;
+		}
+		
+		
+		bw.write(result + "");
+		bw.flush();
+	}
+}
